0382c8fe33ec8aa6e1e0049243d26eae2d341493
Pedro Melo <pedromelo55@gmail.com>
2022-12-05 07:46:04 -0300

    files

---
+++ estudarPilotandoComADOM/DOM.md
@@ -0,0 +1,13 @@
+# DOM (Document Object Model)
+
+* É o HTML convertido para o Objeto Javascript
+* API que representa e interage com o HTML
+* Estrutura de dados do tipo árvore, criado pelo browser
+* Propriedades e métodos
+
+# Para que?
+* Javascript usa a DOM para se conectar ao HTML
+* Manipular o HTML com o Javascript
+* Você só programa para a WEB porque existe a DOM 
+
+* Exemplo
---
+++ estudarPilotandoComADOM/script.js
@@ -0,0 +1,43 @@
+// getElementByID()
+
+const element = document.getElementById("blog-title")
+console.log(element)
+
+/*
+getElementsByClasssName()
+
+const element = document.getElementByClassName("one")
+console.log(element)
+*/
+
+/*
+getElementsByTagName()
+
+const element = document.getElementByTagName("meta")
+console.log(element)
+*/
+
+/*
+querySelector()
+
+const element = document.querySelector(".one")
+console.log(element)
+*/
+
+/*
+querySelectorAll()
+
+const elements = document.querySelectorAll('one')
+
+elements.forEach(el => console.log(el))
+*/
+
+/*
+Qual usar?
+
+getElementByID (element)
+getElementByClassName (HTMLCollection)
+getElementByTagName (HTMLCollection)
+querySelector (element)
+querySelectorAll (Nodelist) -> (pode usar forEach)
+*/
